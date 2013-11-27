c3e901adfba3c6bf198b3b1689e5979fa5147990
Mike Cantelon <mcantelon@gmail.com>
2013-11-27 14:49:05 -0800

    Changed how files are viewed.
    Changed how files are viewed in the backlog viewer.
    
    Changed it so you click to highlight a file then click a button to view
    the file, rather than clicking on its name.

--- src/dashboard/src/media/js/ingest/ingest_file_browser.js
+++ src/dashboard/src/media/js/ingest/ingest_file_browser.js
@@ -16,8 +16,33 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with Archivematica.  If not, see <http://www.gnu.org/licenses/>.
 */
-
 function setupBacklogBrowser(originalsDirectory, arrangeDirectory) {
+
+  var backlogBrowserEntryClickHandler = function(event) {
+    var explorer = event.data.self.explorer
+      , explorerId = explorer.id
+      , entryEl = this
+      , entryId = $(this).attr('id')
+      , borderCssSpec = '1px solid red';
+
+    if (explorer.selectedEntryId == entryId) {
+      // un-highlight selected entry
+      $(entryEl).css('border', '');
+
+      // remove selected entry
+      explorer.selectedEntryId = undefined;
+    } else {
+      // remove highlighting of existing entries
+      $('#' + explorerId).find('.backbone-file-explorer-entry').css('border', '');
+
+      // highlight selected entry
+      $(entryEl).css('border', borderCssSpec);
+
+      // change selected entry
+      explorer.selectedEntryId = entryId; // $(entryEl).attr('id')
+    }
+  }
+
   function moveHandler(move) {
     // don't allow dragging stuff from originals directory?
     if (move.self.id != 'originals') {
@@ -50,7 +75,9 @@ function setupBacklogBrowser(originalsDirectory, arrangeDirectory) {
   var originals = new FileExplorer({
     el: $('#originals'),
     levelTemplate: $('#template-dir-level').html(),
-    entryTemplate: $('#template-dir-entry').html()
+    entryTemplate: $('#template-dir-entry').html(),
+    entryClickHandler: backlogBrowserEntryClickHandler,
+    nameClickHandler: backlogBrowserEntryClickHandler
   });
 
   originals.moveHandler = moveHandler;
@@ -61,35 +88,16 @@ function setupBacklogBrowser(originalsDirectory, arrangeDirectory) {
     el: $('#arrange'),
     levelTemplate: $('#template-dir-level').html(),
     entryTemplate: $('#template-dir-entry').html(),
-    entryClickHandler: function(event) {
-      var explorer = event.data.self.explorer
-        , explorerId = explorer.id
-        , entryEl = this
-        , entryId = $(this).attr('id')
-        , borderCssSpec = '1px solid blue';
-
-      if (explorer.selectedEntryId == entryId) {
-        // un-highlight selected entry
-        $(entryEl).css('border', '');
-
-        // remove selected entry
-        explorer.selectedEntryId = undefined;
-      } else {
-        // remove highlighting of existing entries
-        $('#' + explorerId).find('.backbone-file-explorer-entry').css('border', '');
-
-        // highlight selected entry
-        $(entryEl).css('border', borderCssSpec);
-
-        // change selected entry
-        explorer.selectedEntryId = entryId; // $(entryEl).attr('id')
-      }
-    }
+    entryClickHandler: backlogBrowserEntryClickHandler,
+    nameClickHandler: backlogBrowserEntryClickHandler
   });
 
   arrange.options.actionHandlers = [];
   arrange.moveHandler = moveHandler;
   arrange.refresh(arrangeDirectory);
 
-  return arrange;
+  return {
+    'originals': originals,
+    'arrange': arrange
+  };
 }
